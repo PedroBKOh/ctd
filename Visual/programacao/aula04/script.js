d62aea3fb84ef008621bc7fc06db97ec3c53bc92
PedroBKOh <89111168+PedroBKOh@users.noreply.github.com>
2021-08-25 21:00:39 -0300

    Add files via upload

---
+++ Visual/programacao/aula04/script.js
@@ -0,0 +1,13 @@
+//  Comentário de uma única linha.
+
+/*
+Comentário de múltiplas linhas.
+Utilizada principalmente em documentações de código.
+*/
+
+var saudacao = 'olá';
+var nome = 'Willian';
+
+prompt('Qual o seu nome?');
+
+alert(saudacao + ' ' + nome);
